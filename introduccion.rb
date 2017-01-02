04ae6bfd655c1b504fcc0a0051f41d1a19e547cc
Carlos Jaramillo <cejaramillof@gmail.com>
2017-01-01 19:58:31 -0500

    Introducción

---
+++ introduccion.rb
@@ -0,0 +1,60 @@
+=begin
+  ¿Quién?
+    Yukihiro ‘Matz’ Matsumoto (Japones) 
+
+  ¿Cuándo?
+    Desarrollo desde 1993 - en 1995 Ruby (0.95) 
+
+  ¿Por qué?:
+    Hacer feliz al desarrollador.
+    Filosofía:
+      -No te repitas (Don't repeat yourself, DRY).
+      -Convención sobre Configuración.
+
+  Características:
+    -Terminología orientada al Inglés.
+    -Permite a sus usuarios alterarlo libremente.
+    -Mezcla lo esencial de lenguajes como Ada, Perl, SmalTalk y Phyton.
+    -Ruby es Tipeado y no necesitas declarar variables para usarlas.
+    -Es un lenguaje totalmente orientado a objetos. (En Ruby todo es un objeto).
+
+  ¿Quién?:
+    -Github
+    -Twitter
+    -Justin TV
+    -Amazon
+    -BBC
+    -Cisco
+    -IBM
+    -NASA
+    -Oracle
+    -LimeWire
+    -Yahoo
+
+  ¿Dónde?
+    Descarga:
+      https://www.ruby-lang.org/es/downloads/
+      https://www.ruby-lang.org/es/documentation/installation/
+        -Homebrew (OS X)
+          $ brew install ruby
+        -apt (Debian o Ubuntu)
+          $ sudo apt-get install ruby-full
+            ó
+          $ sudo apt-get install ruby irb rdoc
+        -yum (CentOS, Fedora, or RHEL)
+          $ sudo yum install ruby
+        -portage (Gentoo)
+          $ sudo emerge dev-lang/ruby
+        -pacman (Arch Linux)
+          $ sudo pacman -S ruby
+        -Ruby en Solaris (Sunfreeware - Blastwave) y OpenIndiana (Image Packaging System (IPS))
+          $ pkg install runtime/ruby-18
+      $ ruby -v
+    Programar:
+      Cualquier editor de texto o IDE
+      Desde block de notas (VIM)
+      Netbeans
+      Sublime-text
+      Atom
+      Brackets
+=end
