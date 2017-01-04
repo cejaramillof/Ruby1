0409c773ff2b349c5a9122a622bd4a00dca6018d
Carlos Jaramillo <cejaramillof@gmail.com>
2017-01-04 12:01:25 -0500

    Alcance de las variables

---
+++ alcancevariables.rb
@@ -0,0 +1,31 @@
+=begin
+  El alcance de las variables es el scope, o la visibilidad que va tener la variable es decir en qué rango o en qué partes de nuestro codigo podemos usar dicha variable.
+
+  Existen 3 tipos: 
+    -Globales
+    -Locales
+    -de Instancia
+
+    PD: Los programadores avanzados no acostumbran usar globales porque si tenemos una App muy grande esperando imprimir algo y no imprime eso. tienes que buscar en qué parte de toda la app fue modiciada.
+=end
+
+#Variables Globales: La podemos usar en cualquier parte del código
+  $ejemplo = "Hola codigo global"  
+
+class HolaMundo
+    def initialize()
+    #Variables de Instancia: Solo esta disponible dentro de la clase que fue declarada
+      @ejemplo = "Soy una variable de instancia"      
+    end
+    def saluda()
+    #Variables Locales: Solo esta disponible en el método que fue declarada.
+      ejemplo = "Soy una variable local"
+      
+      puts ejemplo
+      puts $ejemplo
+      puts @ejemplo
+    end
+    objeto = HolaMundo.new()
+    objeto.saluda
+    gets()
+end
